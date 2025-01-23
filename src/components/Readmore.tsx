38ca66e2b3078129562fafeaaf97f722e8537b3d
Matt Garrow <mjkgarrow@gmail.com>
2025-01-23 16:10:17 +1100

    feat: can now add an icon to the readmore

--- src/components/Readmore.tsx
+++ src/components/Readmore.tsx
@@ -1,20 +1,42 @@
 export default function Readmore({
   url,
   description,
-  linkText
+  linkText,
+  iconSrc
 }: {
   url: string
   description: string
   linkText: string
+  iconSrc?: string
 }) {
   return (
-    <p className='my-10 border-y border-neutral-500 py-4 italic'>
-      <span className=''>
-        Readmore: {description}
-        <a href={url} target='_parent' className='text-neutral-200'>
-          {linkText}
-        </a>
-      </span>
-    </p>
+    <>
+      {iconSrc ? (
+        <div className='relative my-10 grid grid-cols-[minmax(5rem,10%)_1fr] items-center gap-4 border-y border-neutral-500 py-4'>
+          <div className='not-prose aspect-square w-full overflow-hidden rounded-md'>
+            <img src={iconSrc} alt='' className='not_fullscreen_media' />
+          </div>
+          <p className='!mb-0 !mt-0 italic'>
+            <span className=''>
+              Readmore: {description}
+              <a href={url} target='_parent' className='text-neutral-200'>
+                {linkText}
+              </a>
+            </span>
+          </p>
+        </div>
+      ) : (
+        <div className='relative my-10 grid items-center gap-4 border-y border-neutral-500 py-4'>
+          <p className='!mb-0 !mt-0 italic'>
+            <span className=''>
+              Readmore: {description}
+              <a href={url} target='_parent' className='text-neutral-200'>
+                {linkText}
+              </a>
+            </span>
+          </p>
+        </div>
+      )}
+    </>
   )
 }
